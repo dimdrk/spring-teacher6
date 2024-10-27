52ec2fc1f0d0657c50252b001838900eaa257d84
DimDrk <dimdrk@gmail.com>
2024-10-27 16:21:59 +0200

    initial commit

---
+++ src/main/java/gr/aueb/cf/teacherapp/service/TeacherService.java
@@ -0,0 +1,54 @@
+package gr.aueb.cf.teacherapp.service;
+
+import gr.aueb.cf.teacherapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.teacherapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.teacherapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.teacherapp.mapper.Mapper;
+import gr.aueb.cf.teacherapp.model.Teacher;
+import gr.aueb.cf.teacherapp.model.static_data.Region;
+import gr.aueb.cf.teacherapp.repository.RegionRepository;
+import gr.aueb.cf.teacherapp.repository.TeacherRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TeacherService implements ITeacherService {
+
+    private final TeacherRepository teacherRepository;
+    private final RegionRepository regionRepository;
+    private final Mapper mapper;
+
+//    ---------------------------------------------------------------------------------------------------------------
+//    @RequiredArgsConstructor
+//
+//    <-------> equals <----------->
+//
+//    @Autowired
+//    public TeacherService(TeacherRepository teacherRepository, RegionRepository regionRepository, Mapper mapper) {
+//        this.teacherRepository = teacherRepository;
+//        this.regionRepository = regionRepository;
+//        this.mapper = mapper;
+//    }
+//    ---------------------------------------------------------------------------------------------------------------
+
+    @Override
+    @Transactional(rollbackOn = Exception.class)
+    public Teacher saveTeacher(TeacherInsertDTO dto)
+            throws EntityAlreadyExistsException, EntityInvalidArgumentException {
+
+        if (teacherRepository.findByVat(dto.getVat()).isPresent()) {
+            throw new EntityAlreadyExistsException("Teacher", "Teacher with vat: " + dto.getVat() + " already exists.");
+        }
+
+        Teacher teacher = mapper.mapToTeacherEntity(dto);
+
+        Region region = regionRepository.findById(dto.getRegionId())
+                .orElseThrow(() -> new EntityInvalidArgumentException("Region", "Invalid region id."));
+
+        teacher.setRegion(region);
+        return teacherRepository.save(teacher);
+    }
+}
