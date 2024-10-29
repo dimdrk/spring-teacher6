df59996aea171109b2ade4bb7845a4f6db43c018
DimDrk <dimdrk@gmail.com>
2024-10-29 21:22:36 +0200

    user updates

--- src/main/java/gr/aueb/cf/teacherapp/service/UserService.java
+++ src/main/java/gr/aueb/cf/teacherapp/service/UserService.java
@@ -4,6 +4,7 @@ import gr.aueb.cf.teacherapp.model.User;
 import gr.aueb.cf.teacherapp.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,9 +14,16 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @Transactional(rollbackOn = Exception.class)
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 }
